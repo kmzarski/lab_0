435a47397a3c238932e756bc1ceda8548e59f24b
Żarski.Kamil <209526@edup.lodz.pl>
2019-03-06 18:06:17 +0100

    TailsGeneretaroe Implementation

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,18 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        ArrayList<String> tailsList = new ArrayList<>();
+        if (!(value == null)) {
+
+            tailsList.add(value);
+            for (int i = 0; i < value.length() + 2; i++) {
+                value = value.substring(1);
+                tailsList.add(value);
+            }
+            tailsList.add("");
+        }
+        return tailsList;
+
     }
 
 }
