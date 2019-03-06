a1ea256a3e2ed3a4817b9933d2ee6d0b9029c3e3
Żarski.Kamil <209526@edup.lodz.pl>
2019-03-06 18:19:26 +0100

    Resolve problem with bad code

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -7,17 +7,13 @@ public class TailsGenerator {
 
     public List<String> tails(String value) {
         ArrayList<String> tailsList = new ArrayList<>();
-        if (!(value == null)) {
-
-            tailsList.add(value);
-            for (int i = 0; i < value.length() + 2; i++) {
-                value = value.substring(1);
-                tailsList.add(value);
+        if (value != null) {
+            for (int i = 0; i < value.length(); i++) {
+                tailsList.add(value.substring(i));
             }
             tailsList.add("");
         }
         return tailsList;
-
     }
 
 }
